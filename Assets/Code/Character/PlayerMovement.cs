e5394ef2f26197ca700506d970d789054f6064d5
Jere <jere.joensuu@tuni.fi>
2022-02-05 16:13:32 +0200

    Add camera rotating

--- Assets/Code/Character/PlayerMovement.cs
+++ Assets/Code/Character/PlayerMovement.cs
@@ -6,17 +6,24 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour {
     
+    GameObject player;
     Rigidbody rb;
+    GameObject camTarget;
     Inputs inputs;
     Vector2 direction;
     public float speed = 5;
+    public float mouseSensitivity = 2;
     public float accelSpeed = 0.0025f;
     public float decelSpeed = 0.005f;
     private float accel = 0;
     private bool moving;
+    private Vector2 cameraRotation;
+    public float cameraHeight;
 
     void Start() {
-        rb = GetComponent<Rigidbody>();
+        player = transform.Find("Model").gameObject;
+        rb = player.GetComponent<Rigidbody>();
+        camTarget = transform.Find("Camera Target").gameObject;
 
         inputs = new Inputs();
         inputs.Enable();
@@ -28,19 +35,30 @@ public class PlayerMovement : MonoBehaviour {
 
     void Update() {
         ApplyMovement();
+        RotateCamera();
     }
 
     void ApplyMovement() {
         if (moving) {
-            Debug.Log("Accelerating");
             accel += accelSpeed;
             if (accel >= 1) accel = 1;
+            RotatePlayer();
         } else {
-            Debug.Log("Decelerating");
             accel -= decelSpeed;
             if (accel <= 0) accel = 0;
         }
-        rb.velocity = new Vector3(direction.x, 0, direction.y) * accel;
+        Vector3 vel = new Vector3(direction.x, 0, direction.y) * accel;
+        rb.velocity = Quaternion.AngleAxis(camTarget.transform.eulerAngles.y, Vector3.up) * vel;
+        camTarget.transform.position = player.transform.position + (Vector3.up * cameraHeight);
+    }
+
+    void RotatePlayer() {
+        // Vector3 current = new Vector3(0, transform.localEulerAngles.y, 0);
+        // Vector3 target = new Vector3(0, transform.localEulerAngles.y + rb.velocity.y, 0);
+        // transform.localEulerAngles = Vector3.RotateTowards(transform.localEulerAngles, transform.localEulerAngles + rb.velocity, 1, 1);
+
+        // float angle = 180 + Vector3.SignedAngle(Vector3.back, rb.velocity, Vector3.up);
+        // player.transform.eulerAngles = new Vector3(player.transform.eulerAngles.x, angle, player.transform.eulerAngles.z);
     }
 
     void ReadMovement(InputAction.CallbackContext context) {
@@ -50,6 +68,15 @@ public class PlayerMovement : MonoBehaviour {
 
     void Jump(InputAction.CallbackContext context) {
 
+    }
+
+    void RotateCamera() {
+        cameraRotation.x += mouseSensitivity * Input.GetAxis("Mouse X");
+
+        cameraRotation.y -= mouseSensitivity * Input.GetAxis("Mouse Y");
+        cameraRotation.y = Mathf.Clamp (cameraRotation.y, -70, 80); // limits vertical rotation
+
+        camTarget.transform.eulerAngles = new Vector3(cameraRotation.y, cameraRotation.x, 0.0f);
     }
 
     void OnDisable() {
