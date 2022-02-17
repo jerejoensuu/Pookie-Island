996ddd03ffdca4cc778f43de39d076efd64dfbb3
Jere <jere.joensuu@tuni.fi>
2022-02-17 21:31:23 +0200

    Store information from vacuum raycasts

--- Assets/Code/VacuumController.cs
+++ Assets/Code/VacuumController.cs
@@ -14,7 +14,9 @@ public class VacuumController : MonoBehaviour {
     List<Vector3> points;
     List<Vector3> targets;
 
-    RaycastHit rayHit;
+    RaycastHit[] rayHits;
+    List<GameObject> hitObjects = new List<GameObject>();
+    List<Vector3> hitPositions = new List<Vector3>();
 
 
     void Start() {
@@ -26,18 +28,32 @@ public class VacuumController : MonoBehaviour {
     }
 
     void CastRays() {
+        hitObjects = new List<GameObject>(); // clear list
+        hitPositions = new List<Vector3>();
         PlotRayPoints();
         Vector3 dir;
         for (int i = 0; i < points.Count; i++) {
             Vector3 origin = points[i];
-            Vector3 target = targets[i] + Vector3.forward * distance;
+            Vector3 target = targets[i] + (Quaternion.AngleAxis(transform.eulerAngles.y, Vector3.up) * Vector3.forward * distance);
             dir = Vector3.Normalize(target - origin);
-            if (Physics.Raycast(origin, dir, out rayHit, distance)) {
-                Debug.DrawLine(origin, rayHit.point, Color.green);
-            } else {
-                Debug.DrawRay(origin, dir * distance, Color.red);
+
+            Debug.DrawRay(origin, dir * distance, new Color(0.5f, 0.5f, 0.5f, 1f));
+            rayHits = Physics.RaycastAll(origin, dir, distance);
+            for (int hit = 0; hit < rayHits.Length; hit++) {
+                StoreHit(rayHits[hit]);
             }
+
+
         }
+    }
+
+    void StoreHit(RaycastHit hit) {
+        GameObject newHitObject = hit.collider.gameObject;
+        foreach (GameObject oldHitObject in hitObjects) {
+            if (oldHitObject == newHitObject) return;
+        }
+        hitObjects.Add(newHitObject);
+        hitPositions.Add(hit.point);
     }
 
     void PlotRayPoints() {
@@ -47,8 +63,10 @@ public class VacuumController : MonoBehaviour {
         int ringNum = 10;
         int outerRing = 360;
 
+        // Center points:
         points.Add(nozzlePos);
-        targets.Add(nozzlePos + Vector3.forward * distance);
+        targets.Add(nozzlePos + (Quaternion.AngleAxis(transform.eulerAngles.y, Vector3.up) * Vector3.forward * distance));
+
         for (int ring = 1; ring < ringNum + 1; ring++) {
             float radius = nozzleSize / ringNum * ring;
             int pointsInRing = Mathf.RoundToInt(outerRing / ringNum * ring * (rayDensity/100));
@@ -65,15 +83,19 @@ public class VacuumController : MonoBehaviour {
                     nozzlePos.y + radius * spread * Mathf.Cos(angle * Mathf.Deg2Rad),
                     nozzlePos.z
                 ));
+                Vector3 dir = targets[targets.Count - 1] - points[points.Count - 1];
+                dir = Quaternion.AngleAxis(transform.eulerAngles.y, Vector3.up) * dir;
+                targets[targets.Count - 1] = Quaternion.AngleAxis(test, Vector3.up) * dir + points[points.Count - 1];
             }
         }
     }
 
     void OnDrawGizmos() {
-        Gizmos.color = Color.red;
+        Gizmos.color = Color.blue;
         if (Application.isPlaying) {
-            foreach (Vector3 point in points) {
-                Gizmos.DrawSphere(point, 0.002f);
+            foreach (Vector3 point in hitPositions) {
+                Debug.Log(point);
+                Gizmos.DrawSphere(point, 0.05f);
             }
         }
         
