0b7553ac1d07d9cba6c3c8fe9a3381b981d551c0
Syrinius <syrinius@gmail.com>
2022-03-22 08:33:27 +0200

    Shard system including prefab and ui update

---
+++ Assets/Code/Shard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+public class Shard : MonoBehaviour {
+
+    private int uid;
+    public static Action<int> onShardPicked;
+
+    private void Awake() {
+        uid = (int)(transform.position.x * 1327 + transform.position.z * 4673);
+    }
+
+    private void Start() {
+        bool contains = SaveUtils.currentSaveGame.PickedShards.Contains(uid);
+        gameObject.SetActive(!contains);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (!other.CompareTag("Player")) return;
+        SaveUtils.currentSaveGame.PickedShards.Add(uid);
+        onShardPicked?.Invoke(++SaveUtils.currentSaveGame.Shards);
+        Destroy(gameObject);
+    }
+}
