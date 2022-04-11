5eb932a44834fb6ddabbfefb5f854d444ed3edee
NédRichard <nedimerichard@gmail.com>
2022-04-11 16:09:28 +0300

    shards shaders/movements tests

--- Assets/Code/Shard.cs
+++ Assets/Code/Shard.cs
@@ -2,7 +2,9 @@
 using UnityEngine;
 
 public class Shard : MonoBehaviour {
-
+    [SerializeField] float speed = 5f;
+    [SerializeField] float height = 0.5f;
+    Vector3 pos;
     private int uid;
     public static Action<int> onShardPicked;
 
@@ -10,10 +12,18 @@ public class Shard : MonoBehaviour {
         uid = (int)(transform.position.x * 1327 + transform.position.z * 4673);
     }
 
+
     private void Start() {
+        pos = transform.position;
         bool contains = SaveUtils.currentSaveGame.PickedShards.Contains(uid);
         gameObject.SetActive(!contains);
     }
+    void Update()
+    {
+        float newY = Mathf.Sin(Time.time * speed) * height + pos.y;
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+    }
+
 
     private void OnTriggerEnter(Collider other) {
         if (!other.CompareTag("Player")) return;
@@ -22,3 +32,4 @@ public class Shard : MonoBehaviour {
         Destroy(gameObject);
     }
 }
+
