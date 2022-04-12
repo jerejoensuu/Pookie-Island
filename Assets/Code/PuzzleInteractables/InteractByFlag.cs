6923a8e0394f4829ce831c3f035acfbf85010bca
Syrinius <syrinius@gmail.com>
2022-04-13 01:23:01 +0300

    Fixed temple quest in Ned test scene, added puzzle interactable by flag

---
+++ Assets/Code/PuzzleInteractables/InteractByFlag.cs
@@ -0,0 +1,18 @@
+﻿public class InteractByFlag : Interactable {
+
+    public string flagToCheck;
+    private void OnEnable() {
+        if (FlagUtils.IsFlagOn(flagToCheck)) OnFlagSet(flagToCheck);
+        else FlagUtils.OnFlagSet += OnFlagSet;
+    }
+
+    private void OnFlagSet(string obj) {
+        if (!obj.Equals(flagToCheck)) return;
+        OnInteraction?.Invoke(this);
+        enabled = false;
+    }
+    
+    private void OnDisable() {
+        FlagUtils.OnFlagSet -= OnFlagSet;
+    }
+}
