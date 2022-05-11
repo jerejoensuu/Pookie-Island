b6df4314cba7e7e4dbe3653e31bda6efaad71c2f
NédRichard <nedimerichard@gmail.com>
2022-05-11 20:05:43 +0300

    shard sfx works + shardExchange code set

--- Assets/Code/ShardCounter.cs
+++ Assets/Code/ShardCounter.cs
@@ -7,7 +7,9 @@ public class ShardCounter : MonoBehaviour {
     // dumb temp code
     public GameObject CrystalCounter;
     private TextMeshProUGUI text;
-    
+    [SerializeField] AudioClip shardSFX;
+    //[SerializeField] AudioClip shardExchangeSFX;
+
     private void Awake() {
         text = GetComponent<TextMeshProUGUI>();
         text.text = SaveUtils.currentSaveGame.Shards.ToString();
@@ -19,12 +21,14 @@ public class ShardCounter : MonoBehaviour {
     }
 
     private void ONShardPicked(int count) {
+        GetComponent<AudioSource>().PlayOneShot(shardSFX);
         if (count % 30 == 0) {
             SaveUtils.currentSaveGame.Exchanges = SaveUtils.currentSaveGame.Exchanges + 1;
             SaveUtils.IncrementCrystal();
             CrystalCounter.GetComponent<TextMeshProUGUI>().text = SaveUtils.currentSaveGame.Crystal.ToString();
         }
         if (SaveUtils.currentSaveGame.Exchanges > 0) {
+            //GetComponent<AudioSource>().PlayOneShot(shardExchangeSFX);
             count = count - (30 * SaveUtils.currentSaveGame.Exchanges);
         }
         text.text = count.ToString();
